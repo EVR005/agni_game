3b2508a1fedf8c4099c4425b4df7d80d27ce1cbf
Edumba Vannia Raja <edumbasankaravel513@gmail.com>
2023-11-28 13:13:49 +0530

    initial content done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./Components/dashboard";
+import Dummy from "./Components/dummy";
+import Navbar from "./Components/navbar";
+import Items from "./Components/items";
+import Comp1 from "./Components/comp1";
+import Comp2 from "./Components/Comp2";
+import Collections from "./Components/collections";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +25,16 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Router>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path="/" element={<Collections />}></Route>
+          <Route path="/comp1" element={<Comp1 />}></Route>
+          <Route path="/comp2" element={<Comp2 />}></Route>
+          <Route path="/items" element={<Items />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/dashboard.js
@@ -0,0 +1,11 @@
+import Navbar from "./navbar";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <div>dashboard</div>
+    </div>
+  );
+};
+
+export default Dashboard;
